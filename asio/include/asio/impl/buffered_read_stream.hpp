91f54a688f10ce14d8fe0195f70549165369608d
Sawalon <sawalon.chen@ui.com>
2022-04-20 23:34:41 +0800

    Squashed 'Sources/ASIO/' changes from a304654bc..a5c5077e4
    a5c5077e4 asio version 1.16.1 released
    3dafb7da4 Revision history.
    4d4dcc901 Add Cirrus CI configuration to test FreeBSD.
    5f23b9efb Update tutorial text to use current resolver API.
    f1ad6e81b Fix incorrect overload selection due to injected class name.
    cc7a4ac9c Replace '\n' with '[br]' in documentation.
    2b7e4fe95 Update POSIX links.
    28d563c7c Remove unused local variable.
    a1336cd56 Use boost.bind placeholders from boost::placeholders namespace.
    6ef3e9544 Call {shutdown,destroy} on priority_scheduler destruction in C++11 example.
    c2cee7860 Fix use of rebind_executor in use_awaitable.as_default_on.
    6f5157978 Regenerate documentation.
    225b3feac Add link to C++14 examples section.
    074e6ff24 Fix comments with incorrect ASIO_HAS_TYPE_TRAITS to use ASIO_HAS_STD_TYPE_TRAITS.
    607f99ba8 Add emscripten compatibility patches (Use O_NONBLOCK instead of FIONBIO + Fix usage of strerror_r).
    46aa35f24 TV titles are also windows apps.
    027a52490 Remove unnecessary null pointer checks.
    19d93672b Call {shutdown,destroy} on priority_scheduler destruction.
    c0d28fc34 Add ssl::context constructor to take ownership of a native handle.
    e6e2ff695 Fix compile error in buffered streams due to lack of reference collapsing in C++98.
    4b37bdbe3 Recreate the socket_select_interrupter's sockets on error.
    63dcf4a98 Fix a Windows-specific thread_pool destructor hang when the pool has an associated I/O object.
    d14f885ca Speed up documentation generation.
    aedb78355 Suppress non-virtual destructor warnings.
    023d36f2d Propagate non-EOF errors from add_certificate_authority.
    9af8d57b2 Remove incorrect handler requirement checks in async_read that were accidentally left behind.
    f44c34c63 Update overview to reflect that handlers are only required to be move constructible.
    2602543e0 Add missing doxygen marker in executor_work_guard documentation.
    f97471a76 Fix build on current NetBSD.
    beb787fd5 Ensure resolver is restarted correctly after a fork.
    f77f97d32 Fix strand<> converting construction and assigment.
    5ab306260 Test calls to buffer_sequence_begin/end.
    41f0c2a02 Add ssl::host_name_verification.
    27083372c Fix work counting issue in asynchronous resolver endpoint implementation.
    992a656e2 Mark posix descriptor classes' move constructors as noexcept.
    8dc44bc23 Add more MSVC versions to appveyor configuration.
    6720d4078 Add MSVC build support for /std:c++latest.
    00157db09 Support C++20 concept syntax.
    8bedc5aa3 When using gcc, use __cplusplus macro to determine language conformance.
    9e1c9c6a6 Fix macro prefix.
    7fba31fb7 Update copyright notices.
    
    git-subtree-dir: Sources/ASIO
    git-subtree-split: a5c5077e44c451ddc8a50c3717e66dcccc92b074

--- asio/include/asio/impl/buffered_read_stream.hpp
+++ asio/include/asio/impl/buffered_read_stream.hpp
@@ -2,7 +2,7 @@
 // impl/buffered_read_stream.hpp
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2003-2019 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -147,7 +147,8 @@ namespace detail
     typedef typename remove_reference<
       Stream>::type::lowest_layer_type::executor_type executor_type;
 
-    explicit initiate_async_buffered_fill(Stream& next_layer)
+    explicit initiate_async_buffered_fill(
+        typename remove_reference<Stream>::type& next_layer)
       : next_layer_(next_layer)
     {
     }
@@ -177,7 +178,7 @@ namespace detail
     }
 
   private:
-    Stream& next_layer_;
+    typename remove_reference<Stream>::type& next_layer_;
   };
 } // namespace detail
 
@@ -364,7 +365,8 @@ namespace detail
     typedef typename remove_reference<
       Stream>::type::lowest_layer_type::executor_type executor_type;
 
-    explicit initiate_async_buffered_read_some(Stream& next_layer)
+    explicit initiate_async_buffered_read_some(
+        typename remove_reference<Stream>::type& next_layer)
       : next_layer_(next_layer)
     {
     }
@@ -403,7 +405,7 @@ namespace detail
     }
 
   private:
-    Stream& next_layer_;
+    typename remove_reference<Stream>::type& next_layer_;
   };
 } // namespace detail
 
--- asio/src/tests/unit/strand.cpp
+++ asio/src/tests/unit/strand.cpp
@@ -2,7 +2,7 @@
 // strand.cpp
 // ~~~~~~~~~~
 //
-// Copyright (c) 2003-2019 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -17,6 +17,7 @@
 #include "asio/strand.hpp"
 
 #include <sstream>
+#include "asio/executor.hpp"
 #include "asio/io_context.hpp"
 #include "asio/dispatch.hpp"
 #include "asio/post.hpp"
@@ -30,7 +31,7 @@
 #endif // defined(ASIO_HAS_BOOST_DATE_TIME)
 
 #if defined(ASIO_HAS_BOOST_BIND)
-# include <boost/bind.hpp>
+# include <boost/bind/bind.hpp>
 #else // defined(ASIO_HAS_BOOST_BIND)
 # include <functional>
 #endif // defined(ASIO_HAS_BOOST_BIND)
@@ -238,8 +239,25 @@ void strand_test()
   ASIO_CHECK(count == 0);
 }
 
+void strand_conversion_test()
+{
+  io_context ioc;
+  strand<io_context::executor_type> s1 = make_strand(ioc);
+
+  // Converting constructors.
+
+  strand<executor> s2(s1);
+  strand<executor> s3 = strand<io_context::executor_type>(s1);
+
+  // Converting assignment.
+
+  s3 = s1;
+  s3 = strand<io_context::executor_type>(s1);
+}
+
 ASIO_TEST_SUITE
 (
   "strand",
   ASIO_TEST_CASE(strand_test)
+  ASIO_COMPILE_TEST_CASE(strand_conversion_test)
 )
